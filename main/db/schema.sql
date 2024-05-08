38199cb364b5c54e9c53c47db3e5064f28627e02
daisyosebe <145105156+daisyosebe@users.noreply.github.com>
2024-05-08 03:50:42 -0500

    seeds file and tables done

--- main/db/schema.sql
+++ main/db/schema.sql
@@ -5,22 +5,21 @@ CREATE DATABASE department_db;
 
 CREATE TABLE department (
   id SERIAL PRIMARY KEY,
-    name: VARCHAR(30) UNIQUE NOT NULL
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
-    title: VARCHAR(30) UNIQUE NOT NULL
-    salary: DECIMAL NOT NULL
-    review TEXT NOT NULL,
-    department_id: INTEGER NOT NULL
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL
 );
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
-    first_name: VARCHAR(30) NOT NULL 
-   last_name: VARCHAR(30)
-    role_id: INTEGER NOT NULL
-    manager_id: INTEGER
-    ON DELETE SET NULL
+    first_name VARCHAR(30) NOT NULL, 
+   last_name VARCHAR(30),
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER 
+    -- ON DELETE SET NULL
 );
---
+++ main/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const routes = require("./routes")
+
+const port = 3001;
+const app = express();
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
