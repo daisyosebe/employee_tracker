8d991cfb8b6950957065f9b95c1f8b1340889ae1
daisyosebe <145105156+daisyosebe@users.noreply.github.com>
2024-05-29 14:07:07 -0500

    added 'drop table if', fixed port number and calling the right function

--- main/db/schema.sql
+++ main/db/schema.sql
@@ -1,50 +1,51 @@
-DROP DATABASE IF EXISTS department_db;
-CREATE DATABASE department_db;
+-- Drop tables if they exist to avoid conflicts
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
 
-\c department_db;
-
-CREATE TABLE department (
+-- Create departments table
+CREATE TABLE departments (
   id SERIAL PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(30) UNIQUE NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL
+-- Create roles table with foreign key reference to departments table
+CREATE TABLE roles (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INTEGER NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-CREATE TABLE employee (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL, 
-   last_name VARCHAR(30),
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER REFERENCES employee(id) ON DELETE SET NULL
-    
+-- Create employees table with foreign key references to roles and employees (for manager)
+CREATE TABLE employees (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30),
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER ,
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
 
-INSERT INTO department (name)
-VALUES ('Business'),
-       ('HR'),
-       ('Finance');
+-- Initial data into departments table
+INSERT INTO departments (name)
+VALUES 
+  ('Business'),
+  ('HR'),
+  ('Finance');
 
-INSERT INTO role (title, salary, department_id)
-VALUES ('manager', 200000, 1 ),
-       ('assistant', 30000, 2 ),
-       ('CEO', 500000, 3);
-       
+--Initial data into roles table
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+  ('manager', 200000.00, 1),
+  ('assistant', 30000.00, 2),
+  ('CEO', 500000.00, 3);
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ('john', 'Doe',1),
-       ('Rachel', 'Adams',2),
-       ('Chris', 'Rock',3);
-
-SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, manager.last_name AS manager
-FROM employee
-JOIN role ON employee.role_id = role.id
-JOIN department ON department.id = role.department_id
-JOIN employee AS manager ON employee.manager_id = manager.id 
-
-
-
+-- Insert initial data into employees table
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES 
+  ('John', 'Doe', 1, NULL),
+  ('Rachel', 'Adams', 2, NULL),
+  ('Chris', 'Rock', 3, 1);
--- main/server.js
+++ main/server.js
@@ -2,249 +2,237 @@ const { Client } = require('pg');
 const inquirer = require('inquirer');
 
 const client = new Client({
-  user: '',
+  user: 'postgres',  
   host: 'localhost',
   database: 'department_db',
   password: 'daisy',
-  port: 3001,
+  port: 5432,  // Correct PostgreSQL port
 });
 
 client.connect()
   .then(() => console.log('Connected to the database'))
   .catch(err => console.error('Connection error', err.stack));
 
-  function tracker() {
-    inquirer.prompt([
-      {
-        type: 'list',
-        name: 'action',
-        message: 'What would you like to do?',
-        choices: [
-          'View All Employees',
-          'Add Employee',
-          'Update Employee Role',
-          'View All Roles',
-          'Add Role',
-          'View All Departments',
-          'Add Department',
-          'Exit'
-        ]
-      }
-    ]).then((answers) => {
-      switch (answers.action) {
-        case 'View all Employees':
+function tracker() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View All Employees',
+        'Add Employee',
+        'Update Employee Role',
+        'View All Roles',
+        'Add Role',
+        'View All Departments',
+        'Add Department',
+        'Exit'
+      ]
+    }
+  ]).then((answers) => {
+    switch (answers.action) {
+      case 'View All Employees':
         viewEmployees();
         break;
-        case 'Add Employee':
+      case 'Add Employee':
         addEmployee();
         break;
-        case 'Update Employee Role':
+      case 'Update Employee Role':
         updateEmployeeRole();
         break;
-        case 'View All Roles':
+      case 'View All Roles':
         viewRoles();
         break;
-        case 'Add Role':
+      case 'Add Role':
         addRole();
         break;
-        case 'View All Departments':
+      case 'View All Departments':
         viewDepartments();
         break;
-        case 'Add Department':
+      case 'Add Department':
         addDepartment();
         break;
-        default:
-          client.end();
-          break;
-      }
-    });
-  }
+      default:
+        client.end();
+        break;
+    }
+  });
+}
 
-// TODO: VIEW EOMPLOYEES
+// VIEW EMPLOYEES
 function viewEmployees() {
-    const query = `
-      SELECT employees.*, roles.title AS job_title, roles.salary, departments.name AS department_name,
-      CONCAT(managers.first_name, ' ', managers.last_name) AS manager_name
-      FROM employees
-      LEFT JOIN roles ON employees.role_id = roles.id
-      LEFT JOIN departments ON roles.department_id = departments.id
-      LEFT JOIN employees managers ON employees.manager_id = managers.id;
-    `;
-    client.query(query, (err, res) => {
-      if (err) {
-        console.error('Query error', err.stack);
-      } else {
-        console.table(res.rows);
-        start();
-      }
-    });
-  }
-  
+  const query = `
+    SELECT employees.*, roles.title AS job_title, roles.salary, departments.name AS department_name,
+    CONCAT(managers.first_name, ' ', managers.last_name) AS manager_name
+    FROM employees
+    LEFT JOIN roles ON employees.role_id = roles.id
+    LEFT JOIN departments ON roles.department_id = departments.id
+    LEFT JOIN employees AS managers ON employees.manager_id = managers.id;
+  `;
+  client.query(query, (err, res) => {
+    if (err) {
+      console.error('Query error', err.stack);
+    } else {
+      console.table(res.rows);
+      tracker();
+    }
+  });
+}
 
-//TODO: ADD EMPLOYEE
+// ADD EMPLOYEE
 function addEmployee() {
-    inquirer.prompt([
-      {
-        type: 'input',
-        name: 'firstName',
-        message: 'Enter the first name of the employee:'
-      },
-      {
-        type: 'input',
-        name: 'lastName',
-        message: 'Enter the last name of the employee:'
-      },
-      {
-        type: 'input',
-        name: 'roleId',
-        message: 'Enter the role ID for the employee:'
-      },
-      {
-        type: 'input',
-        name: 'managerId',
-        message: 'Enter the manager ID for the employee (leave blank if none):',
-        default: null
-      }
-    ]).then((answers) => {
-      client.query(
-        'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)',
-        [answers.firstName, answers.lastName, answers.roleId, answers.managerId],
-        (err) => {
-          if (err) {
-            console.error('Query error', err.stack);
-          } else {
-            console.log('Employee added!');
-            start();
-          }
-        }
-      );
-    });
-  }
-  
-  
-//TODO: UPDATE EMPLOYEE ROLE
-function updateEmployeeRole() {
-    inquirer.prompt([
-      {
-        type: 'input',
-        name: 'employeeId',
-        message: 'Enter the ID of the employee you want to update:'
-      },
-      {
-        type: 'input',
-        name: 'newRoleId',
-        message: 'Enter the new role ID for the employee:'
-      }
-    ]).then((answers) => {
-      client.query(
-        'UPDATE employees SET role_id = $1 WHERE id = $2',
-        [answers.newRoleId, answers.employeeId],
-        (err) => {
-          if (err) {
-            console.error('Query error', err.stack);
-          } else {
-            console.log('Employee role updated!');
-            start();
-          }
-        }
-      );
-    });
-  }
-  
-
-// TODO: VIEW ROLES
-function viewRoles() {
-    const query = `
-      SELECT roles.*, departments.name AS department_name
-      FROM roles
-      JOIN departments ON roles.department_id = departments.id;
-    `;
-    client.query(query, (err, res) => {
-      if (err) {
-        console.error('Query error', err.stack);
-      } else {
-        console.table(res.rows);
-        start();
-      }
-    });
-  }
-  
-
-// TODO: ADD ROLE
-function addRole() {
-    inquirer.prompt([
-      {
-        type: 'input',
-        name: 'roleTitle',
-        message: 'Enter the title of the new role:'
-      },
-      {
-        type: 'input',
-        name: 'roleSalary',
-        message: 'Enter the salary for the new role:'
-      },
-      {
-        type: 'input',
-        name: 'roleDepartment',
-        message: 'Enter the department ID for the new role:'
-      }
-    ]).then((answers) => {
-      client.query(
-        'INSERT INTO roles (title, salary, department_id) VALUES ($1, $2, $3)',
-        [answers.roleTitle, answers.roleSalary, answers.roleDepartment],
-        (err) => {
-          if (err) {
-            console.error('Query error', err.stack);
-          } else {
-            console.log('Role added!');
-            start();
-          }
-        }
-      );
-    });
-  }
-  
-
-// TODO: VIEW DEPARTMENTS
-function viewEmployees() {
-    const query = `
-      SELECT employees.*, roles.title AS job_title, roles.salary, departments.name AS department_name,
-      CONCAT(managers.first_name, ' ', managers.last_name) AS manager_name
-      FROM employees
-      LEFT JOIN roles ON employees.role_id = roles.id
-      LEFT JOIN departments ON roles.department_id = departments.id
-      LEFT JOIN employees managers ON employees.manager_id = managers.id;
-    `;
-    client.query(query, (err, res) => {
-      if (err) {
-        console.error('Query error', err.stack);
-      } else {
-        console.table(res.rows);
-        start();
-      }
-    });
-  }
-  
-
-// TODO: ADD DEPARTMENT
-function addDepartment() {
-    inquirer.prompt([
-      {
-        type: 'input',
-        name: 'departmentName',
-        message: 'Enter the name of the new department:'
-      }
-    ]).then((answer) => {
-      client.query('INSERT INTO departments (name) VALUES ($1)', [answer.departmentName], (err) => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'firstName',
+      message: 'Enter the first name of the employee:'
+    },
+    {
+      type: 'input',
+      name: 'lastName',
+      message: 'Enter the last name of the employee:'
+    },
+    {
+      type: 'input',
+      name: 'roleId',
+      message: 'Enter the role ID for the employee:'
+    },
+    {
+      type: 'input',
+      name: 'managerId',
+      message: 'Enter the manager ID for the employee (leave blank if none):',
+      default: null
+    }
+  ]).then((answers) => {
+    client.query(
+      'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)',
+      [answers.firstName, answers.lastName, answers.roleId, answers.managerId],
+      (err) => {
         if (err) {
           console.error('Query error', err.stack);
         } else {
-          console.log('Department added!');
-          start();
+          console.log('Employee added!');
+          tracker();
         }
-      });
+      }
+    );
+  });
+}
+
+// UPDATE EMPLOYEE ROLE
+function updateEmployeeRole() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'employeeId',
+      message: 'Enter the ID of the employee you want to update:'
+    },
+    {
+      type: 'input',
+      name: 'newRoleId',
+      message: 'Enter the new role ID for the employee:'
+    }
+  ]).then((answers) => {
+    client.query(
+      'UPDATE employees SET role_id = $1 WHERE id = $2',
+      [answers.newRoleId, answers.employeeId],
+      (err) => {
+        if (err) {
+          console.error('Query error', err.stack);
+        } else {
+          console.log('Employee role updated!');
+          tracker();
+        }
+      }
+    );
+  });
+}
+
+// VIEW ROLES
+function viewRoles() {
+  const query = `
+    SELECT roles.*, departments.name AS department_name
+    FROM roles
+    JOIN departments ON roles.department_id = departments.id;
+  `;
+  client.query(query, (err, res) => {
+    if (err) {
+      console.error('Query error', err.stack);
+    } else {
+      console.table(res.rows);
+      tracker();
+    }
+  });
+}
+
+// ADD ROLE
+function addRole() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'roleTitle',
+      message: 'Enter the title of the new role:'
+    },
+    {
+      type: 'input',
+      name: 'roleSalary',
+      message: 'Enter the salary for the new role:'
+    },
+    {
+      type: 'input',
+      name: 'roleDepartment',
+      message: 'Enter the department ID for the new role:'
+    }
+  ]).then((answers) => {
+    client.query(
+      'INSERT INTO roles (title, salary, department_id) VALUES ($1, $2, $3)',
+      [answers.roleTitle, answers.roleSalary, answers.roleDepartment],
+      (err) => {
+        if (err) {
+          console.error('Query error', err.stack);
+        } else {
+          console.log('Role added!');
+          tracker();
+        }
+      }
+    );
+  });
+}
+
+// VIEW DEPARTMENTS
+function viewDepartments() {
+  const query = `
+    SELECT * FROM departments;
+  `;
+  client.query(query, (err, res) => {
+    if (err) {
+      console.error('Query error', err.stack);
+    } else {
+      console.table(res.rows);
+      tracker();
+    }
+  });
+}
+
+// ADD DEPARTMENT
+function addDepartment() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'departmentName',
+      message: 'Enter the name of the new department:'
+    }
+  ]).then((answer) => {
+    client.query('INSERT INTO departments (name) VALUES ($1)', [answer.departmentName], (err) => {
+      if (err) {
+        console.error('Query error', err.stack);
+      } else {
+        console.log('Department added!');
+        tracker();
+      }
     });
-  }
-  
+  });
+}
 
 tracker();
